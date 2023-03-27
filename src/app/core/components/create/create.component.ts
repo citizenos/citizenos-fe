6bb69865187132fdbeff8a61764a9c79c1ce3701
ilmar <ilmar.tyrk@gmail.com>
2023-03-27 22:53:23 +0300

    Update desktop nav

--- src/app/core/components/create/create.component.ts
+++ src/app/core/components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-create',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent {
+  constructor (public translate: TranslateService) {
 
+  }
 }
