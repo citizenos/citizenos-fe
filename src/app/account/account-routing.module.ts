e0ab9a246c8a633171808d981b9b3c7bd20970e6
ilmar <ilmar.tyrk@gmail.com>
2023-04-12 17:44:20 +0300

    Login page and form

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -4,15 +4,15 @@ import { HomeComponent } from 'src/app/core/components/home/home.component';
 import { AuthGuard } from '../auth/auth.guard';
 import { AccountComponent } from './account.component';
 import { LoginComponent } from './components/login/login.component';
-import { LoginDialogComponent } from './components/login/login.component';
 import { PasswordResetDialogComponent } from './components/password-reset/password-reset.component';
 import { RegisterDialogComponent } from './components/register/register.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: '', canActivate: [AuthGuard], component: AccountComponent },
   {
     path: '', component: HomeComponent, children: [
-      { path: 'login', component: LoginDialogComponent },
+      { path: 'login', component: LoginComponent },
       { path: 'signup', component: RegisterDialogComponent },
       { path: 'password/reset/:passwordResetCode', component: PasswordResetDialogComponent },
       { path: 'signup', component: RegisterDialogComponent }
