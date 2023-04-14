7b9950e56bacd48d20035a906b2d0a7f9e310e19
ilmar <ilmar.tyrk@gmail.com>
2023-04-14 11:01:50 +0300

    WIP register and login views

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -5,17 +5,18 @@ import { AuthGuard } from '../auth/auth.guard';
 import { AccountComponent } from './account.component';
 import { LoginComponent } from './components/login/login.component';
 import { PasswordResetDialogComponent } from './components/password-reset/password-reset.component';
-import { RegisterDialogComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
   { path: '', canActivate: [AuthGuard], component: AccountComponent },
   {
     path: '', component: HomeComponent, children: [
       { path: 'login', component: LoginComponent },
-      { path: 'signup', component: RegisterDialogComponent },
+      { path: 'signup', component: RegisterComponent },
       { path: 'password/reset/:passwordResetCode', component: PasswordResetDialogComponent },
-      { path: 'signup', component: RegisterDialogComponent }
+      { path: 'signup', component: RegisterComponent }
     ]
   },
 ];
---
+++ src/app/account/components/register-form/register-form.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { ConfigService } from 'src/app/services/config.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/services/notification.service';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { take } from 'rxjs';
+import { UntypedFormGroup, UntypedFormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss']
+})
+export class RegisterFormComponent {
+  config: any;
+  signUpForm = new UntypedFormGroup({
+    name: new UntypedFormControl('', Validators.required),
+    email: new UntypedFormControl('', Validators.email),
+    company: new UntypedFormControl(''),
+    password: new UntypedFormControl('', Validators.required),
+    passwordConfirm: new UntypedFormControl('', Validators.required),
+    agreeToTerms: new UntypedFormControl(false, Validators.requiredTrue)
+  })
+
+  isInviteFlowSignUp = false;
+  wWidth = window.innerWidth;
+  errors = <any>{};
+  agreeToTerms = false;
+  termsVersion: number;
+  preferences = {
+    showInSearch: false
+  }
+  redirectSuccess?:string = '';
+  constructor(private dialog: MatDialog, ConfigService: ConfigService, private AuthService: AuthService, private Notification: NotificationService) {
+    this.config = ConfigService.get('legal');
+    this.termsVersion = this.config.termsVersion;
+    /*if (data.email) {
+      this.signUpForm.patchValue({'email': data.email});
+    }
+    if (data.redirectSuccess) {
+      this.redirectSuccess = data.redirectSuccess;
+    }*/
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleTerms() {
+    this.signUpForm.patchValue({agreeToTerms: this.agreeToTerms})
+  }
+
+  doSignUp() {
+    const formData = this.signUpForm.value;
+    if (!this.agreeToTerms) {
+      this.errors = {
+        terms: 'MSG_ERROR_NEED_TO_AGREE_TERMS'
+      }
+      return;
+    }
+
+    if (formData.password && formData.password !== formData.passwordConfirm) {
+      this.errors = {
+        password: 'MSG_ERROR_PASSWORD_MISMATCH'
+      };
+      return;
+    } else {
+      this.AuthService
+        .signUp({
+          email:formData.email,
+          password: formData.password,
+          name: formData.name,
+          company: formData.company,
+          redirectSuccess: this.redirectSuccess,
+          preferences: this.preferences,
+          termsVersion: this.termsVersion
+        }).pipe(take(1))
+        .subscribe({
+          next: (response:any) => {
+            this.dialog.closeAll(); // Close all dialogs, including the one open now...
+            if (response.data && response.redirectSuccess) {
+              window.location.href = response.redirectSuccess;
+            } else {
+              this.Notification.addInfo('MSG_INFO_CHECK_EMAIL_TO_VERIFY_YOUR_ACCOUNT');
+            }
+          },
+          error: (res) => {
+            this.errors = res.errors;
+          }
+        })
+    }
+  };
+}
