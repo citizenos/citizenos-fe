d11a0c9227b68846c5b490c743b2507cea6904e3
ilmar <ilmar.tyrk@gmail.com>
2023-04-18 11:22:33 +0300

    Change lgoin/register layout  WIP

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -7,20 +7,25 @@ import { LoginComponent } from './components/login/login.component';
 import { PasswordResetDialogComponent } from './components/password-reset/password-reset.component';
 import { RegisterComponent } from './components/register/register.component';
 import { PasswordForgotComponent } from './components/password-forgot/password-forgot.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: RegisterComponent },
-  { path: 'password/forgot', component: PasswordForgotComponent },
-  { path: '', canActivate: [AuthGuard], component: AccountComponent },
   {
-    path: '', component: HomeComponent, children: [
+    path: '', component: AccountComponent, children: [
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: RegisterComponent },
-      { path: 'password/reset/:passwordResetCode', component: PasswordResetDialogComponent },
-      { path: 'signup', component: RegisterComponent }
+      { path: 'password/forgot', component: PasswordForgotComponent },
     ]
   },
+  { path: '', canActivate: [AuthGuard], component: ProfileComponent },
+  /* {
+     path: '', component: HomeComponent, children: [
+       { path: 'login', component: LoginComponent },
+       { path: 'signup', component: RegisterComponent },
+       { path: 'password/reset/:passwordResetCode', component: PasswordResetDialogComponent },
+       { path: 'signup', component: RegisterComponent }
+     ]
+   },*/
 ];
 
 @NgModule({
