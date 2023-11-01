0de7385b8196397b01f782a44f6b206bbce43527
ilmar <ilmar.tyrk@gmail.com>
2023-11-01 13:16:58 +0200

    FIX https://github.com/citizenos/citizenos-fe/issues/255

--- src/app/topic/components/argument-report-moderate/argument-report-moderate.component.ts
+++ src/app/topic/components/argument-report-moderate/argument-report-moderate.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TopicArgumentService } from 'src/app/services/topic-argument.service';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Argument } from 'src/app/interfaces/argument';
-import { switchMap, take } from 'rxjs';
+import { switchMap, take, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-argument-report-moderate',
@@ -13,13 +13,17 @@ import { switchMap, take } from 'rxjs';
 export class ArgumentReportModerateComponent implements OnInit {
   argument!: Argument;
   reportTypes = Object.keys(this.TopicArgumentService.ARGUMENT_REPORT_TYPES);
+  topicId!: string;
+  token!: string;
   report: any = {
     type: '',
     text: ''
   };
   errors?: any;
-  constructor(@Inject(MAT_DIALOG_DATA) data: any, private TopicArgumentService: TopicArgumentService) {
+  constructor(@Inject(MAT_DIALOG_DATA) data: any, private TopicArgumentService: TopicArgumentService, private dialog: MatDialog) {
+    this.token = data.token;
     this.argument = data.argument;
+    this.topicId = data.topicId;
     this.report = data.report;
   }
 
@@ -29,16 +33,20 @@ export class ArgumentReportModerateComponent implements OnInit {
   doModerate() {
 /*TODO resove data */
     const data = {
-     /* token: this.token,
-      topicId: this.topic.id,
+      token: this.token,
+      topicId: this.topicId,
       commentId: this.argument.id,
-      reportId: this.reportId*/
+      reportId: this.report.id,
       report: this.report
     }
     this.TopicArgumentService.moderate(data).pipe(take(1))
     .subscribe({
-      next:() => {},
-      error: () => {}
+      next:() => {
+        this.dialog.closeAll();
+      },
+      error: (err) => {
+        console.log(err);
+      }
     })
   };
 
@@ -49,18 +57,26 @@ export class ArgumentReportModerateComponent implements OnInit {
   template: '',
 })
 export class ArgumentReportModerateDialogComponent {
-  constructor(dialog: MatDialog, route: ActivatedRoute, TopicArgumentService: TopicArgumentService) {
+  topicId: string = '';
+  token: string = '';
+  constructor(dialog: MatDialog, router: Router, route: ActivatedRoute, TopicArgumentService: TopicArgumentService) {
     /*TODO resove queryParam token */
-    route.params.pipe(switchMap((params) => {
-      return TopicArgumentService.getReport({
-        topicId: params['topicId'],
-        commentId: params['commentId'],
-        reportId: params['reportId'],
-        token: params['token']
+    combineLatest([route.params, route.queryParams]).pipe(switchMap(([params, query]) => {
+      this.topicId = params['topicId'];
+      this.token = query['token'];
+        return TopicArgumentService.getReport({
+          topicId: params['topicId'],
+          commentId: params['commentId'],
+          reportId: params['reportId'],
+          token: query['token']
       });
     })).pipe(take(1))
       .subscribe((report) => {
-        dialog.open(ArgumentReportModerateComponent, { data: { report } });
+        const reportDialog = dialog.open(ArgumentReportModerateComponent, { data: { report: report, argument: report.comment, topicId: this.topicId, token: this.token } });
+
+        reportDialog.afterClosed().subscribe((confirm) => {
+          return router.navigate(['/']);
+        })
       });
   }
 
