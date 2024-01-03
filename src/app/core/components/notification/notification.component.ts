c8bfc0512f3cb16371a357364b26b0f716d4b45e
ilmar <ilmar.tyrk@gmail.com>
2024-01-03 16:54:48 +0200

    FIX notification class

--- src/app/core/components/notification/notification.component.ts
+++ src/app/core/components/notification/notification.component.ts
@@ -24,10 +24,6 @@ export class NotificationComponent implements OnInit {
     this.changeDetection.detectChanges();
   }
 
-  getOpenDialogsLength() {
-    return this.dialogs.openDialogs.length;
-  }
-
   trackByIndex(index: number): number {
     return index;
   }
@@ -40,5 +36,12 @@ export class NotificationComponent implements OnInit {
         this.notifications.addWarning('PLEASE NOTE! This Citizen OS is a testing environment and is for TESTING ONLY, all data here MAY be deleted at any time! Please visit <a href="https://app.citizenos.com">https://app.citizenos.com</a> for LIVE application.');
       }, 1000)
     }
+  }
+  getNotificationClass(level?: string) {
+    let classList = [level];
+    if (this.dialogs.openDialogs.length) {
+      classList.push('narrow');
+    }
+    return classList;
   }
 }
