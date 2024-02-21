a33ad7ef71f6fc311b368f9bdba3b713744567c6
ilmar <ilmar.tyrk@gmail.com>
2024-02-21 22:48:41 +0200

    FIX https://github.com/citizenos/citizenos-fe/issues/795

--- src/app/core/components/notification/notification.component.ts
+++ src/app/core/components/notification/notification.component.ts
@@ -31,11 +31,11 @@ export class NotificationComponent implements OnInit {
   ngAfterViewInit(): void {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
-    if(this.showTestingEnvNotification) {
+  /*  if(this.showTestingEnvNotification) {
       setTimeout(() => {
         this.notifications.addWarning('PLEASE NOTE! This Citizen OS is a testing environment and is for TESTING ONLY, all data here MAY be deleted at any time! Please visit <a href="https://app.citizenos.com">https://app.citizenos.com</a> for LIVE application.');
       }, 1000)
-    }
+    }*/
   }
   getNotificationClass(level?: string) {
     let classList = [level];
