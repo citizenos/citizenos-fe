84c9aab4b5bf383dc9790f387b86cea5d3bd027a
ilmar <ilmar.tyrk@gmail.com>
2024-02-23 13:07:30 +0200

    Add prebuild dist

--- prebuildDist.js
+++ prebuildDist.js
@@ -5,14 +5,15 @@ const fs = require('fs');
 const path = require('path');
 
 const configFe = config.util.loadFileConfigs(__dirname + '/src/assets/config');
-const pathSettings = path.resolve('./src/assets/config/config.json');
+const pathDistSettings = path.resolve(__dirname + '/dist/citizenos-fe/assets/config/config.json');
+
 try {
   /*  const settingsFileTxt = '(function (window) { window.__config = window.__config || {};';
     _(configFe).forEach(function (value, key) {
         settingsFileTxt += ' window.__config.' + key + ' = ' + JSON.stringify(value) + ';';
     });
     settingsFileTxt += '}(this));';*/
-    fs.writeFileSync(pathSettings, JSON.stringify(configFe));
+    fs.writeFileSync(pathDistSettings, JSON.stringify(configFe));
 } catch (err) {
     console.log('config.ts write FAILED to ' + pathSettings, err);
     process.exit(1);
