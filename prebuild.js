f4b8b41d1c5cff6a7d21d75364153cf77db8e420
ilmar <ilmar.tyrk@gmail.com>
2024-02-23 13:03:31 +0200

    Alter prebuild.js

--- prebuild.js
+++ prebuild.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const path = require('path');
 
 const configFe = config.util.loadFileConfigs(__dirname + '/src/assets/config');
-const pathSettings = path.resolve('./src/assets/config/config.json');
-const pathDistSettings = path.resolve('./dist/citizenos-fe/assets/config/config.json');
+const pathSettings = path.resolve(__dirname + '/src/assets/config/config.json');
+const pathDistSettings = path.resolve(__dirname + '/dist/citizenos-fe/assets/config/config.json');
 
 try {
   /*  const settingsFileTxt = '(function (window) { window.__config = window.__config || {};';
