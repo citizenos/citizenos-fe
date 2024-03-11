18f1e4fb8967fef7f623c13ad84c0674a64b6bfe
ilmar <ilmar.tyrk@gmail.com>
2024-03-11 18:07:19 +0200

    FIX https://github.com/citizenos/citizenos-fe/issues/967

--- src/app/account/components/login/login.component.ts
+++ src/app/account/components/login/login.component.ts
@@ -251,7 +251,7 @@ export class LoginDialogComponent extends LoginComponent{
         }
       }
     });
-
+    console.log(this.data.redirectSuccess);
     if (this.data.redirectSuccess) {
       this.redirectSuccess = this.data.redirectSuccess;
     }
