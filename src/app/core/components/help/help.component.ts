139829881e0eea1b745085f177e630af9dddb740
ilmar <ilmar.tyrk@gmail.com>
2024-03-11 11:33:05 +0200

    Add help request API call

--- src/app/core/components/help/help.component.ts
+++ src/app/core/components/help/help.component.ts
@@ -1,10 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { tap } from 'rxjs';
+import { take, tap } from 'rxjs';
 import { AppService } from 'src/app/services/app.service';
 import { ConfigService } from 'src/app/services/config.service';
+import { LocationService } from 'src/app/services/location.service';
+import { NotificationService } from 'src/app/services/notification.service';
 import { TourService } from 'src/app/services/tour.service';
 
 @Component({
@@ -21,10 +24,19 @@ export class HelpComponent implements OnInit {
 
   helpForm = new UntypedFormGroup({
     email: new UntypedFormControl('', [Validators.required, Validators.email]),
-    message: new UntypedFormControl('', Validators.required),
+    description: new UntypedFormControl('', Validators.required),
   });
 
-  constructor(public sanitizer: DomSanitizer, public app: AppService, private config: ConfigService, private TourService: TourService, private router: Router) {
+  constructor(
+    public sanitizer: DomSanitizer,
+    public app: AppService,
+    private config: ConfigService,
+    private TourService: TourService,
+    private router: Router,
+    private Location: LocationService,
+    private Notification: NotificationService,
+    private http: HttpClient
+  ) {
     this.app.showHelp.pipe(tap((show) => {
       const url = this.router.url;
       this.showTourBox = false;
@@ -65,7 +77,7 @@ export class HelpComponent implements OnInit {
     }
   }
 
-  startTour () {
+  startTour() {
     this.toggleHelp();
     window.scrollTo(0, 0);
     if (window.innerWidth > 560) {
@@ -75,6 +87,29 @@ export class HelpComponent implements OnInit {
   }
 
   sendHelpRequest() {
-
+    if (!this.helpForm.invalid) {
+      let path = this.Location.getAbsoluteUrlApi('/api/internal/help');
+      let mailData = Object.assign(this.helpForm.value,{
+        userAgent: window.navigator.userAgent,
+        // @ts-ignore:next-line
+        platform: window.navigator.platform || window.navigator.userAgentData?.platform,
+        height: window.innerHeight,
+        width: window.innerWidth,
+        location: this.Location.currentUrl()
+      });
+      this.http.post(path, mailData, { withCredentials: true, observe: 'body', responseType: 'json' }).pipe(
+        take(1),
+      ).subscribe({
+        next: (res) => {
+          this.helpForm.reset();
+          this.toggleHelp();
+          this.Notification.addSuccess('HELP_WIDGET.MSG_REQUEST_SENT');
+        },
+        error: (err) => {
+          console.error(err);
+          this.Notification.addError(err.message);
+        }
+      });
+    }
   }
 }
