b612fc7a743a408a0c73a09a540dca5dd46b82b0
ilmar <ilmar.tyrk@gmail.com>
2024-04-05 00:35:51 +0200

    Request flow fixes

--- src/app/group/components/topic-requests/topic-requests.component.ts
+++ src/app/group/components/topic-requests/topic-requests.component.ts
@@ -4,6 +4,7 @@ import { Component, Inject } from '@angular/core';
 import { Group } from 'src/app/interfaces/group';
 import { DIALOG_DATA } from 'src/app/shared/dialog';
 import { GroupRequestTopicService } from 'src/app/services/group-request-topic.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-topic-requests',
@@ -13,7 +14,7 @@ import { GroupRequestTopicService } from 'src/app/services/group-request-topic.s
 export class TopicRequestsComponent {
   group: Group;
   request$: Observable<any[]>;
-  constructor(@Inject(DIALOG_DATA) data: any, public GroupRequestTopicService: GroupRequestTopicService, private GroupService: GroupService) {
+  constructor(@Inject(DIALOG_DATA) data: any, public GroupRequestTopicService: GroupRequestTopicService, private GroupService: GroupService, private Notification: NotificationService) {
     this.group = data.group;
     this.request$ = GroupRequestTopicService.getItems({groupId: data.group.id}).pipe(map((res) => res.rows));
 
@@ -22,13 +23,17 @@ export class TopicRequestsComponent {
   accept(request: any) {
     const reqData = Object.assign({requestId: request.id}, request);
     this.GroupRequestTopicService.accept(reqData).pipe(take(1)).subscribe(() => {
+      this.Notification.removeAll();
+      this.Notification.addSuccess('COMPONENTS.TOPIC_REQUESTS.MSG_ACCEPT_SUCCESS');
       this.GroupRequestTopicService.reloadItems();
-      this.GroupService.reloadGroup()
+      this.GroupService.reloadGroup();
     });
   }
   reject(request: any) {
     const reqData = Object.assign({requestId: request.id}, request);
     this.GroupRequestTopicService.reject(reqData).pipe(take(1)).subscribe(() => {
+      this.Notification.removeAll();
+      this.Notification.addSuccess('COMPONENTS.TOPIC_REQUESTS.MSG_REJECT_SUCCESS');
       this.GroupRequestTopicService.reloadItems();
       this.GroupService.reloadGroup()
     });
