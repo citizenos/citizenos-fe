0a22dcc2f25b1e86d36943895de32e3e29f757fd
ilmar <ilmar.tyrk@gmail.com>
2024-05-03 13:27:16 +0300

    Update user language preference from language selectori

--- src/app/core/components/language-select/language-select.component.ts
+++ src/app/core/components/language-select/language-select.component.ts
@@ -1,8 +1,11 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ConfigService } from 'src/app/services/config.service';
 import { Router, PRIMARY_OUTLET } from '@angular/router';
 import { DialogRef } from 'src/app/shared/dialog';
+import { UserService } from 'src/app/services/user.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-language-select',
@@ -11,7 +14,13 @@ import { DialogRef } from 'src/app/shared/dialog';
 })
 export class LanguageSelectComponent implements OnInit {
   languages$: { [key: string]: any } = this.config.get('language').list;
-  constructor(public translate: TranslateService, public config: ConfigService, private router: Router, private dialogRef: DialogRef<LanguageSelectComponent>,) {
+  constructor(
+    public translate: TranslateService,
+    public config: ConfigService,
+    private router: Router,
+    private dialogRef: DialogRef<LanguageSelectComponent>,
+    private AuthService: AuthService,
+    private User: UserService) {
   }
 
   ngOnInit(): void {
@@ -24,6 +33,9 @@ export class LanguageSelectComponent implements OnInit {
 
     let g = outlet?.segments.map(seg => seg.path) || [''];
     g.splice(0, 1);
+    if (this.AuthService.loggedIn$) {
+      this.User.updateLanguage(lang).pipe(take(1)).subscribe();
+    }
     this.router.navigate(g, { queryParams: parsedUrl.queryParams, fragment: parsedUrl.fragment || undefined });
     this.dialogRef.close();
   }
