be0f569417c2fe17a5400de5c79bd3cec3829d1b
ilmar <ilmar.tyrk@gmail.com>
2024-06-21 01:06:45 +0300

    FIX https://github.com/citizenos/citizenos-fe/issues/1319

--- src/app/topic/components/argument/argument.component.ts
+++ src/app/topic/components/argument/argument.component.ts
@@ -21,6 +21,7 @@ import { ArgumentReportComponent } from '../argument-report/argument-report.comp
 export class ArgumentComponent implements OnInit {
   @ViewChild('argumentBody') argumentBody!: ElementRef;
   @Input() argument!: any;
+  @Input() canReply!: any;
   @Input() root?: any;
   @Input() topicId!: string;
   @Input() showReplies?:boolean = false;
