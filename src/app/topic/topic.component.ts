1b4fa026db3da8108aabdb06917fc6b395b5635b
ilmar <ilmar.tyrk@gmail.com>
2024-07-25 16:24:38 +0300

    FIX https://github.com/citizenos/citizenos-fe/issues/1471

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -177,6 +177,7 @@ export class TopicComponent implements OnInit {
     this.app.darkNav = true;
     this.tabSelected$ = combineLatest([this.route.fragment, this.route.queryParams]).pipe(
       map(([value, params]) => {
+        if (this.router.url.indexOf('/ideation/') > -1) return 'ideation';
         if (params['folderId']) return 'ideation';
         if (params['argumentId']) return 'discussion';
       /*  if (this.hideDiscussion === true && value === 'discussion') {
