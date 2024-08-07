fc710c29845cccb750e121961c7b5d02b5218271
ilmar <ilmar.tyrk@gmail.com>
2024-08-07 14:17:53 +0300

    FIX https://github.com/citizenos/citizenos-fe/issues/1486

--- src/app/topic/components/topic-join/topic-join.component.ts
+++ src/app/topic/components/topic-join/topic-join.component.ts
@@ -1,7 +1,7 @@
 import { TopicService } from 'src/app/services/topic.service';
 import { Component, Inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap, take, tap } from 'rxjs';
+import { switchMap, take, tap, combineLatest } from 'rxjs';
 import { TopicJoinService } from 'src/app/services/topic-join.service';
 import { LocationService } from 'src/app/services/location.service';
 import { Topic } from 'src/app/interfaces/topic';
@@ -30,60 +30,58 @@ export class TopicTokenJoinComponent {
   token: string = '';
   join$;
   constructor(router: Router, dialog: DialogService, route: ActivatedRoute, Location: LocationService, TopicJoinService: TopicJoinService, Auth: AuthService, app: AppService, TopicService: TopicService) {
-    this.join$ = Auth.user$.pipe(take(1)).subscribe((user:any) => {
-    if (!user) {
-      app.doShowLogin(Location.getAbsoluteUrl(router.url));
-    } else if (user && !Auth.loggedIn$.value) {
-
-    } else {
-      route.params.pipe(
-        switchMap((params: any) => {
-          this.token = params['token'];
-          return TopicJoinService
-            .getByToken(params['token'])
-        }),
-        take(1)
-      ).subscribe({
-        next: (topic) => {
-          if (topic.id && (topic.permission.level !== 'none' || topic.visibility === TopicService.VISIBILITY.public )) {
-            router.navigate(['topics', topic.id]);
-          }
-          const joinDialog = dialog.open(TopicJoinComponent, {
-            data: {
-              topic: topic
-            }
-          });
-          joinDialog.afterClosed().subscribe((confirm) => {
-            if (confirm === true) {
-              TopicJoinService.join(this.token).pipe(
-                take(1)
-              ).subscribe({
-                next: (topic) => {
-                  TopicService.reloadTopic();
-                  router.navigate(['topics', topic.id]);
-                },
-                error: (res) => {
-                  const status = res.status;
-                  if (status.code === 40100 && !Auth.loggedIn$.value) { // Unauthorized
-                    const currentUrl = Location.getAbsoluteUrl(router.url);
-                    router.navigate(['/account/login'], { queryParams: { redirectSuccess: currentUrl } });
-                  } else if (status.code === 40001) { // Matching token not found.
-                    router.navigate(['/']);
-                  } else {
-                    router.navigate(['/404']);
-                  }
+    this.join$ = combineLatest([Auth.loggedIn$, Auth.user, route.params]).pipe(take(1), tap(([loggedIn, user, routeParams]: any) => {
+      if (!user || user.id === null) {
+        app.doShowLogin(Location.getAbsoluteUrl(router.url));
+      } else if (user && !loggedIn) {
+        console.log(user, loggedIn)
+      } else {
+        this.token = routeParams['token'];
+        TopicJoinService
+          .getByToken(this.token)
+          .subscribe({
+            next: (topic) => {
+              if (topic.id && (topic.permission.level !== 'none' || topic.visibility === TopicService.VISIBILITY.public)) {
+                router.navigate(['topics', topic.id]);
+              }
+              const joinDialog = dialog.open(TopicJoinComponent, {
+                data: {
+                  topic: topic
                 }
-              })
-            } else if (topic.visibility !== TopicService.VISIBILITY.public) {
-              router.navigate(['/']);
+              });
+              joinDialog.afterClosed().subscribe((confirm) => {
+                if (confirm === true) {
+                  TopicJoinService.join(this.token).pipe(
+                    take(1)
+                  ).subscribe({
+                    next: (topic) => {
+                      TopicService.reloadTopic();
+                      router.navigate(['topics', topic.id]);
+                    },
+                    error: (res) => {
+                      const status = res.status;
+                      if (status.code === 40100 && !Auth.loggedIn$.value) { // Unauthorized
+                        const currentUrl = Location.getAbsoluteUrl(router.url);
+                        router.navigate(['/account/login'], { queryParams: { redirectSuccess: currentUrl } });
+                      } else if (status.code === 40001) { // Matching token not found.
+                        router.navigate(['/']);
+                      } else {
+                        router.navigate(['/404']);
+                      }
+                    }
+                  })
+                } else if (topic.visibility !== TopicService.VISIBILITY.public) {
+                  router.navigate(['/']);
+                }
+              });
+            },
+            error: (err) => {
+              console.error('JOIN ERROR', err);
             }
-          });
-        },
-        error: (err) => {
-          console.log(err);
-        }
-      });}
+          })
+      }
+    })).subscribe({
+      error: (err) => console.error('Token join error', err)
     });
-
   }
 }
