801a1c613b33b0b5121155c79d6a2b42dc74fa6a
ilmar <ilmar.tyrk@gmail.com>
2024-09-26 18:38:17 +0300

    FIX https://github.com/citizenos/citizenos-fe/issues/1611

--- src/app/topic/components/topic-form/topic-form.component.ts
+++ src/app/topic/components/topic-form/topic-form.component.ts
@@ -77,6 +77,16 @@ export class TopicFormComponent {
       this.topicText = content;
       if (content.nativeElement.offsetHeight >= 320) {
         this.readMoreButton.next(true);
+      } else {
+        const contentChilds = content.nativeElement.children[2]?.children;
+        let h = 0;
+        for(let i=0; i<contentChilds.length; i++) {
+          h+=contentChilds[i].offsetHeight;
+          if (h >= 320) {
+            this.readMoreButton.next(true);
+            i = contentChilds.length;
+          }
+        }
       }
       this.cd.detectChanges();
     }
