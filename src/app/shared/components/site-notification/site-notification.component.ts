b72695a796133c9483654a78c8e736d1b86668fd
Ilmar Türk <ilmar.tyrk@gmail.com>
2024-12-03 23:11:14 +0200

    Add notification message

---
+++ src/app/shared/components/site-notification/site-notification.component.ts
@@ -0,0 +1,30 @@
+import { CookieService } from 'ngx-cookie-service';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { ConfigService } from '@services/config.service';
+
+import { NotificationService } from '@services/notification.service';
+
+@Component({
+  selector: 'site-notification',
+  templateUrl: './site-notification.component.html',
+  styleUrl: './site-notification.component.scss'
+})
+export class SiteNotificationComponent {
+  public showNotification = false;
+  constructor(
+    private readonly config: ConfigService,
+    private readonly CookieService: CookieService,
+    private readonly translate: TranslateService,
+    private readonly Notification: NotificationService) {
+      if (this.config.get('showIssueNotification') && !this.CookieService.get('show-issue-notification')) {
+        this.showNotification = true;
+      }
+  }
+
+  close() {
+    this.showNotification = false;
+    this.CookieService.set('show-issue-notification', 'true', 36500);
+  }
+}
