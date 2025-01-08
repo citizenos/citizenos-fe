487c948c194bb667cd37e3fa25aecb63051bc49e
Viktar Maslouski <diver.vm@gmail.com>
2025-01-08 14:57:39 +0100

    update topics and groups

--- src/app/public-topics/components/topics/topics.component.ts
+++ src/app/public-topics/components/topics/topics.component.ts
@@ -222,8 +222,16 @@ export class TopicsComponent implements OnInit {
     const filtersShow = Object.entries(this.mobileFilters).find(([key, value]) => {
       return !!value;
     })
-    if (filtersShow)
-      this.mobileFilters[filtersShow[0]] = false;
+    if (filtersShow) {
+      const filterName = filtersShow[0]
+      this.mobileFilters[filterName] = false;
+      if (filterName === 'language') {
+        this.languageSearch$.next(this.languageSearch);
+      }
+      if (filterName === 'country') {
+        this.countrySearch$.next(this.countrySearch);
+      }
+    }
   }
 
   showMobileOverlay() {
