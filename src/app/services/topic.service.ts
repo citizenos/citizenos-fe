0a59d98b8361bbfd658f74e888103d4de0b421e0
Viktar Maslouski <diver.vm@gmail.com>
2025-01-09 22:57:33 +0100

    fix types

--- src/app/services/topic.service.ts
+++ src/app/services/topic.service.ts
@@ -13,7 +13,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class TopicService {
-  public CATEGORIES = <any>{
+  public CATEGORIES = {
     agriculture: "agriculture",
     animal_protection: "animal_protection",
     arts: "arts",
