b5c13154a07367da4f81655e4da64da0f37bf7bc
Viktar Maslouski <diver.vm@gmail.com>
2025-02-09 14:39:13 +0100

    wip common invitation dialog

--- src/app/interfaces/dialogdata.ts
+++ src/app/interfaces/dialogdata.ts
@@ -1,8 +1,33 @@
-export interface InviteData{
-  invite: any
-  currentUrl?: string
+/**
+ * @note Deprecated
+ */
+export interface InviteData {
+  invite: any;
+  currentUrl?: string;
+}
+
+export interface InviteDialogData {
+  imageUrl: string | undefined;
+  title: string;
+  intro: string | undefined;
+  description: string | undefined;
+  creator: {
+    imageUrl: string | undefined;
+    name: string;
+  };
+  user: {
+    email: string;
+    isRegistered: boolean;
+  };
+  level: string;
+  visibility: string;
+  currentUrl: string;
+  publicAccess: {
+    title: string;
+    link: string[];
+  } | null;
 }
 
 export interface ReportData {
-  topic:any
+  topic: any;
 }
