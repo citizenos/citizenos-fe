1f10c16870c68640606a9127781f239a855d7fed
Ilmar Türk <ilmar.tyrk@gmail.com>
2025-02-17 21:36:23 +0200

    Resolve conflicts

--- src/app/ideation/components/ideation-create/ideation-create.component.ts
+++ src/app/ideation/components/ideation-create/ideation-create.component.ts
@@ -79,10 +79,7 @@ export class IdeationCreateComponent extends TopicFormComponent implements Block
     question: '',
     deadline: null,
     disableReplies: false,
-<<<<<<< HEAD
-=======
     allowAnonymous: false,
->>>>>>> master
     createdAt: '',
     updatedAt: ''
   };
