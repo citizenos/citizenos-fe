4dfaa2c20ee4e077ba3578af526192f8e7dd56cb
Viktar Maslouski <diver.vm@gmail.com>
2025-03-11 13:33:38 +0300

    fix restricting numbers

--- src/app/ideation/components/add-idea/add-idea.component.ts
+++ src/app/ideation/components/add-idea/add-idea.component.ts
@@ -204,6 +204,14 @@ export class AddIdeaComponent {
     }
   }
 
+  numberOnly(event: any) {
+    const charCode = event.which ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
+
   ngModelBlur(key: string) {
     this.ideaForm.controls[key].markAsUntouched();
   }
