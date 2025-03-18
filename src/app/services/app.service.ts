7157f1b2a942df438a0bce60ce539ca66767aa34
Viktar Maslouski <diver.vm@gmail.com>
2025-03-18 17:49:46 +0300

    fix logout

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -121,7 +121,7 @@ export class AppService {
         next: () => {
           this.AuthService.reloadUser();
           setTimeout(() => {
-            this.router.navigate(['/']);
+            this.router.navigate(['/', this.translate.currentLang]);
           });
         },
         error: (err) => {
