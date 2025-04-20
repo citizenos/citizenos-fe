3d60547b929c8cba5fad79ab75b97341ba2c7247
Viktar Maslouski <diver.vm@gmail.com>
2025-04-20 11:32:32 +0200

    add autosave loaders and styles

--- src/app/ideation/components/add-idea/add-idea.component.ts
+++ src/app/ideation/components/add-idea/add-idea.component.ts
@@ -499,7 +499,6 @@ export class AddIdeaComponent {
     if (isAutosave) {
       this.isAutosaving = true;
     }
-
     if (this.autosavedIdea) {
       this.TopicIdeaService.update(ideaData).subscribe({
         next: (idea) => {
